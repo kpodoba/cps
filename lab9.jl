deb9ce84bd46e1cb68f9cab8ee6252d2d69a3f51
mmm4m <makin2243@gmail.com>
2024-05-28 16:53:19 +0200

    Add files via upload

---
+++ lab9.jl
@@ -0,0 +1,44 @@
+## Problem 9.1: Zaimplementuj funkcję firwin_lp_I: {2𝐿 + 1 : 𝐿 ∈ ℕ} × 
+## ℝ → ℝ^(2𝐿+1), którazwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿+1)
+## nierekursywnego filtru dolnoprzepustowego o unormowanej częstotliwości 
+## odcięca 𝑓0 ∈ ℝ, oraz liniowej charakterystyce fazowe.
+## Do zaprojektowania filtru wykorzystaj metodę okien czasowych oraz okno 
+## prostokątne.
+
+
+## Problem 9.2: Zaimplementuj funkcję firwin_hp_I: {2𝐿 + 1 : 𝐿 ∈ ℕ} ×
+## ℝ → ℝ^(2𝐿+1), która zwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿+1)
+## nierekursywnego filtru górnoprzepustowego o unormowanej częstotliwości
+## odcięca 𝑓0 ∈ ℝ, oraz liniowej charakterystyce fazowe. Do zaprojektowania
+## filtru wykorzystaj metodę okien czasowych oraz okno prostokątne.
+
+
+## Problem 9.3: Zaimplementuj funkcję firwin_bp_I: {2𝐿 + 1 : 𝐿 ∈ ℕ} × 
+## ℝ → ℝ^(2𝐿+1), która zwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿+1)
+## nierekursywnego filtru pasmowoprzepustowego o unormowanych częstotliwościach
+## odcięca 𝑓0, 𝑓1 ∈ ℝ, gdzie 𝑓0 < 𝑓1, oraz liniowej charakterystyce fazowe.
+## Do zaprojektowania filtru wykorzystaj metodę okien czasowych oraz okno
+## prostokątne.
+
+
+## Problem 9.4: Zaimplementuj funkcję firwin_bs_I: {2𝐿 + 1 : 𝐿 ∈ ℕ} × 
+## ℝ → ℝ^(2𝐿+1), która zwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿+1) nierekursywn
+## -ego filtru pasmowozaporowego o unormowanych częstotliwościach odcięca 𝑓0, 
+## 𝑓1 ∈ ℝ, gdzie 𝑓0 < 𝑓1, oraz liniowej charakterystyce fazowe.
+## Do zaprojektowania filtru wykorzystaj metodę okien czasowych oraz okno prostokątne.
+
+
+## Problem 9.5: Zaimplementuj funkcję firwin_lp_II: {2𝐿 : 𝐿 ∈ ℕ} × 
+## ℝ → ℝ^(2𝐿), która zwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿) nierekursywnego
+## filtru dolnoprzepustowego o unormowanej
+## częstotliwości odcięca 𝑓0 ∈ ℝ, oraz liniowej charakterystyce fazowe.
+## Do zaprojektowania filtru wykorzystaj metodę okien czasowych oraz okno 
+## prostokątne.
+
+
+## Problem 9.6: Zaimplementuj funkcję firwin_bp_II: {2𝐿 : 𝐿 ∈ ℕ} ×
+## ℝ → ℝ^(2𝐿), która zwróci odpowiedź impulsową ℎ ∈ ℝ^(2𝐿) nierekursywnego
+## filtru pasmowoprzepustowego o unormowanych częstotliwościach odcięca 𝑓0,
+## 𝑓1 ∈ ℝ, gdzie 𝑓0 < 𝑓1, oraz liniowej charakterystyce fazowe.
+## Do zaprojektowania filtru wykorzystaj metodę okien czasowych oraz okno
+## prostokątne.
